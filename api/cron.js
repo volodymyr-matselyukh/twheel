14e6367855c1a648c22285c4a4a495683f8910fb
Volodymyr Matseliukh <volodymyr.matseliukh@gigatechspace.com>
2024-02-27 19:26:51 +0100

    fix import

--- api/cron.js
+++ api/cron.js
@@ -1,3 +1,5 @@
+import { spinWheelSingleTime } from '../index.js';
+
 export default async function handler(req, res) {
     const result = await spinWheelSingleTime();
     res.status(200).end(result);
